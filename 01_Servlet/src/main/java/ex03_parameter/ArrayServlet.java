d62b688cfb332cc64a4292c2397b7aa44356da28
pona8257 <pona8257@naver.com>
2023-03-16 18:34:39 +0900

    2023-03-16

---
+++ 01_Servlet/src/main/java/ex03_parameter/ArrayServlet.java
@@ -0,0 +1,36 @@
+package ex03_parameter;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/ArrayServlet")
+public class ArrayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 요청 인코딩
+		request.setCharacterEncoding("UTF-8");
+		
+		// 요청 파라미터
+		String[] tel = request.getParameterValues("tel");
+		
+		
+		
+		response.getWriter().append("tel: ").append(tel[0] + "-" + tel[1] + "-" + tel[2]);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
