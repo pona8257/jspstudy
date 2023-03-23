1edd9a91b840eff4c73db2dfc07e3d1f42129a4f
pona8257 <pona8257@naver.com>
2023-03-23 17:35:38 +0900

    2023-03-23

---
+++ 01_Servlet/src/main/java/ex07_ajax/AgeHandleException.java
@@ -0,0 +1,12 @@
+package ex07_ajax;
+
+public class AgeHandleException extends MyHandleException{
+	private static final long serialVersionUID = 4128045155784813008L;
+	
+	public AgeHandleException(String message, int errorCode) {
+		super(message, errorCode);
+		
+	}
+
+	
+}
---
+++ 01_Servlet/src/main/java/ex08_api/AirKoreaApiServlet.java
@@ -0,0 +1,92 @@
+package ex08_api;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+
+@WebServlet("/AirKoreaApiServlet")
+public class AirKoreaApiServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 요청 인코딩
+		request.setCharacterEncoding("UTF-8");
+		
+		// 요청 파라미터
+		String sidoName = request.getParameter("sidoName");
+		String returnType = request.getParameter("returnType");
+		
+		// 서비스키
+		String serviceKey = "l/CL7ud+rPyAXX5leg3cZ041yAVSwuBnYAh1DQeZVT7uShqVoQrj+aCEQTOuy01ujpEYcXDKepCQdE2IlFvWUw==";
+		
+		// 요청 주소
+		String apiURL = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty";
+		apiURL += "?serviceKey=" + URLEncoder.encode(serviceKey, "UTF-8");
+		apiURL += "&sidoName=" +  URLEncoder.encode(sidoName, "UTF-8");	// 한글이여서 안하면 깨진다
+		apiURL += "&returnType=" + URLEncoder.encode(returnType, "UTF-8");
+		
+		// URL
+		URL url = new URL(apiURL);
+		
+		// HttpURLConnection
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+	 	
+		// 요청 메소드
+		con.setRequestMethod("GET");
+		
+		// returnType에 따른 Content-Type
+		con.setRequestProperty("content-Type", "application/" + returnType + "; charset=UTF-8");
+		
+		// 입력 스트림 생성
+		BufferedReader reader = null;
+		if(con.getResponseCode() == 200) {
+			reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		} else {
+			reader = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+		}
+		
+		// 입력 (API의 응답 결과를 StringBulider에 저장)
+		StringBuilder sb = new StringBuilder();
+		String line = null;
+		while((line= reader.readLine()) != null) {
+			sb.append(line);
+		}
+		
+		// 입력 스트림, 접속 종료
+		reader.close();
+		con.disconnect();
+		
+		// API 결과를 ajax 응답 처리하기
+		response.setContentType("application/" + returnType + "; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println(sb.toString());
+		out.flush();
+		out.close();
+		System.out.println(sb.toString());
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
