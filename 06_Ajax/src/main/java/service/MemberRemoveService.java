ba8853570f5d626c7075a30b877342a643abe1ee
pona8257 <pona8257@naver.com>
2023-04-07 17:30:50 +0900

    2023-04-07

---
+++ 06_Ajax/src/main/java/service/MemberRemoveService.java
@@ -0,0 +1,14 @@
+package service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MemberRemoveService implements IMemberService {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
